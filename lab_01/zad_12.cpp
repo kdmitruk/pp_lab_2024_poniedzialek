e6a2152e070d67849a60a44e168dda058257d611
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-07 16:09:22 +0200

    zad12

--- lab_01/zad_12.cpp
+++ lab_01/zad_12.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    float m0, mr, v, l, roznica;
+    cout << "Podaj mase poczatkowa: " << endl;
+    cin >> m0;
+    cout << "Podaj predkosc: " << endl;
+    cin >> v;
+    l=sqrt(1-pow(v,2)/pow(299792458.f,2));
+    mr = m0/l;
+    roznica = mr-m0;
+    cout << roznica << endl;
     return 0;
 }
