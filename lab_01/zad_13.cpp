bcd22949b2d46c34c2a62ed7a9f2ca44c5323cd6
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-07 16:35:05 +0200

    z13

--- lab_01/zad_13.cpp
+++ lab_01/zad_13.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
-
+#include<cmath>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    u_int64_t t1= 2250 , t2;
+    int r1 = 1971, r2 = 2024;
+    t2= t1 * pow(2,(r2-r1)/2);
+    cout<< t2<<endl;
     return 0;
 }
