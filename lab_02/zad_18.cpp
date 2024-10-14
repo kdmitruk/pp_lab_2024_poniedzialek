7d5b3900b5d72f21258e5abafada9fb04f01ba00
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-14 16:40:58 +0200

    z18

--- lab_02/zad_18.cpp
+++ lab_02/zad_18.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
-
 using namespace std;
+enum Weekdays{Saturday, Sunday,Monday, Tuesday, Wednesday, Thursday, Friday};
 
 int main()
 {
-    cout << "Hello World!" << endl;
+
+    int day, month, year;
+    //scanf("%d.%d.%d",&day, &month, &year);
+    day=14;
+    month=10;
+    year=2024;
+    if (month == 1 || month == 2) {
+        month += 12;
+    }
+    int k = year%100;
+    int j = year/100;
+    int d = (day + 13*(month+1)/5 + k +k/4 + j/4 - 2*j) % 7;
+    Weekdays h=(Weekdays)d;
+    switch (h){
+    case Monday: cout<<"Poniedzialek"; break;
+    case Tuesday: cout<<"Wtorek"; break;
+    case Wednesday: cout<<"Sroda"; break;
+    case Thursday: cout<<"Czwartek"; break;
+    case Friday: cout<<"Piatek"; break;
+    case Saturday: cout<<"Sobota"; break;
+    case Sunday: cout<<"Niedziela"; break;
+    }
     return 0;
 }
