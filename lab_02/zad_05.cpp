72e4f9883ffd5b232ec01e4792f2b984b55c6471
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-14 15:53:42 +0200

    z5 z9

--- lab_02/zad_05.cpp
+++ lab_02/zad_05.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    float a1, b1, a2, b2;
+    b1 = 1;
+    b2 = 2;
+    a1 = 3;
+    a2 = 4;
+    cout << ((a2 < b1)||(a1 > b2) ? "no ":"") << "intersection" << endl;
+
+
     return 0;
 }
--- lab_02/zad_09.cpp
+++ lab_02/zad_09.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    double a, b, c;
+    cout << "Podaj Liczbę A" << endl;
+    cin >> a;
+    cout << "Podaj Liczbę B" << endl;
+    cin >> b;
+    cout << "Podaj Liczbę C" << endl;
+    cin >> c;
+    // if(a + b > c && a + c > b && b + c > a){
+    if(a + b <= c || a + c <= b || b + c <= a){
+        cout << "Nie mozna zrobić trójkąta" << endl;
+    }else{
+        cout << "Można zrobić trójkąt" << endl;
+
+    }
     return 0;
 }
