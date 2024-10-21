56ac8dc9992d790d4ad6f9764cbea6cc4c797b8b
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-21 16:40:32 +0200

    z18

--- lab_03/zad_18.cpp
+++ lab_03/zad_18.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
 using namespace std;
-
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+    int n = 100;
+    bool prime;
+    for(int i=2;i<n;i++){
+        prime=true;
+        for(int j=2;j<=i/2;j++)
+            if(i%j==0)
+                prime=false;
+        if(prime)
+            printf("%d ",i);
+    }
     return 0;
 }
