2d890757e0f0dc2bbe66e43b597a6e0a1664a997
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-21 15:31:57 +0200

    11

--- lab_03/zad_11.cpp
+++ lab_03/zad_11.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int rows = 10, cols = 9;
+    bool flag;
+    bool newline_flag = true;
+    for(int y = 0; y < rows; ++y){
+        flag = newline_flag;
+        for(int x = 0; x < cols; ++x){
+            cout << (flag == true ? "#":"o");
+            flag = !flag;
+        }
+        std::cout << "\n";
+        newline_flag = ! newline_flag;
+    }
+
+
     return 0;
 }
