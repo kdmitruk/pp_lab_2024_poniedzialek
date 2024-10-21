2ad0cdeb1a52063e8a38edb5faa9856f4f738683
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-21 15:58:05 +0200

    z 13

--- lab_03/zad_13.cpp
+++ lab_03/zad_13.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int liczba = 457, suma = 0;
+
+    while(liczba != 0){
+        suma += liczba %10;
+        liczba = liczba / 10;
+    }
+
+    cout << "suma cyfr naszej liczby wynosi " << suma << endl;
 }
