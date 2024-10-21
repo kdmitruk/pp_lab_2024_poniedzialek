1bc83ea3eddb1b94a6080ee7f4403a4aac7c2089
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-21 14:17:06 +0200

    z. 5,6

--- lab_03/zad_05.cpp
+++ lab_03/zad_05.cpp
@@ -2,8 +2,26 @@
 
 using namespace std;
 
+/*int main()
+{
+    float x, sum=0;
+    cout<<"Podaj liczbe: "<<endl;
+    cin>>x;
+    while(x!=0){
+        sum+=x;
+        cin>>x;
+    }
+    cout<<sum;
+    return 0;
+}*/
 int main()
 {
-    cout << "Hello World!" << endl;
+    float x, sum=0;
+    cout<<"Podaj liczbe: "<<endl;
+    do{
+        cin>>x;
+        sum+=x;
+    }while(x!=0);
+    cout<<sum;
     return 0;
 }
--- lab_03/zad_06.cpp
+++ lab_03/zad_06.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    float x, sum=0;
+    do{
+        cin>>x;
+        sum+=x;
+    }while(sum<100);
+    cout<<sum-100;
     return 0;
 }
