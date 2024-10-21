7f4fdf95d2134a7323cd745715f658ee11f4403e
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-21 14:02:19 +0200

    z3

--- lab_03/zad_03.cpp
+++ lab_03/zad_03.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 
-using namespace std;
-
 int main()
 {
-    cout << "Hello World!" << endl;
+    int n, num, sum = 0;
+    float avg;
+    std::cin >> n;
+    for(int i = 0; i < n; i++) {
+        std::cin >> num;
+        sum += num;
+    }
+    avg = sum/(float)n;
+    std::cout << avg;
     return 0;
 }
