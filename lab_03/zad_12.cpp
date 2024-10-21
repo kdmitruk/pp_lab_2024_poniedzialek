fc657f2fa69638a2550059ec8aa695af6a0aa528
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-21 15:47:20 +0200

    :):)

--- lab_03/zad_12.cpp
+++ lab_03/zad_12.cpp
@@ -4,6 +4,15 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    int liczba = 222, cyfry = 0;
+    if(liczba == 0){
+        cyfry = 1;
+    }else{
+    //while(liczba != 0){
+    //     liczba = liczba / 10;
+    //     cyfry++;
+        for(; liczba != 0; cyfry++, liczba/=10);
+
+}
+    cout << "Liczba cyfr naszej liczby wynosi " << cyfry << endl;
 }
