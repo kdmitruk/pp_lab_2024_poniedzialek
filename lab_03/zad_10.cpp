11e473f764f94cf2e03e0ec37453373a0324cf40
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-21 15:02:52 +0200

    no i fajne zadanie

--- lab_03/zad_10.cpp
+++ lab_03/zad_10.cpp
@@ -4,6 +4,21 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int height;
+    cout << "Podaj Wysokość Choinki" << endl;
+    cin >> height;
+    int space_count;
+    int star_count;
+    for(int i = 0; i < height ; ++i){
+        space_count = height - i - 1;
+        star_count = i * 2 + 1;
+        for(int j = 0 ;j < space_count ; ++j){
+            cout << " ";
+        }
+        for(int j = 0 ;j < star_count ; ++j ){
+            cout << "*";
+        }
+        cout << "\n";
+    }
     return 0;
 }
