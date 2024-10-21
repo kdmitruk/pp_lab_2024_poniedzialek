44bbd6f716d0be93501f6135a1069a12bfe73564
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-21 14:09:48 +0200

    z4

--- lab_03/zad_04.cpp
+++ lab_03/zad_04.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
+#include <climits>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int n, tmp, biggest_number = INT_MIN;
+    cin >> n;
+    for(int i = 0; i < n; ++i){
+        cin >> tmp;
+        if(biggest_number < tmp) biggest_number = tmp;
+    }
+    cout << biggest_number << "\n";
     return 0;
 }
