a124da47c95e52b2b29c216c38d3821410d4d555
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-21 14:27:46 +0200

    7

--- lab_03/zad_07.cpp
+++ lab_03/zad_07.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    for(int i = 1; i <= 10; ++i){
+        for(int j = 1; j <= 10; ++j){
+            std::cout << std::setw(3) << i * j << " ";
+        }
+        std::cout << "\n";
+    }
+
     return 0;
 }
