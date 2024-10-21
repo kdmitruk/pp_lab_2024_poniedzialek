9213aa437586778b889edf23c8767dbbbe1541a8
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-21 16:10:57 +0200

    :)))

--- lab_03/zad_14.cpp
+++ lab_03/zad_14.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int temp, pl = 0, dl = 1, n = 20;
+    for(int i = 0;i < n ;i++){
+        cout << pl << " ";
+        temp = dl;
+        dl = pl + dl;
+        pl = temp;
+    }
     return 0;
 }
