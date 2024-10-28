bc6b0c5aa743c906df950858655f00b0f830d85c
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-28 16:39:46 +0100

    16

--- lab_04/zad_16.cpp
+++ lab_04/zad_16.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
-
+#include <cstring>
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const int size = 20;
+    char array[size] = "Jan";
+    char tmp[size];
+    // strcpy(tmp, array);
+    {
+        int i = 0;
+        for(; array[i] != '\0'; ++i){
+            tmp[i] = array[i];
+        }
+        tmp[i] = '\0';
+        cout << tmp << "\n";
+    }
+
+    // strncpy(tmp, array, size);
+
+    {
+        int i = 0;
+        for(; array[i] != '\0' && i < size; ++i){
+            tmp[i] = array[i];
+        }
+        tmp[i] = '\0';
+        cout << tmp << "\n";
+    }
+
+
     return 0;
 }
