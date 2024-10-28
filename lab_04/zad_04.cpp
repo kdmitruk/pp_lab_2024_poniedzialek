f2e64b1bf5cb3809e769d304d366f91ff3eb7a95
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-28 14:41:33 +0100

    Zad 4,5

--- lab_04/zad_04.cpp
+++ lab_04/zad_04.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <iomanip>
 using namespace std;
+int main() {
+    int arr[5];
+    srand(time(0));
+    for(int i=0;i<5;i++)
+        arr[i]=rand()%10+1;
+    for(int i=0;i<5;i++)
+        cout<<arr[i]<<' ';
+    cout<<endl;
+    int sum=0, prod=1;
+    float sum2=0;
+    for(int i=0;i<5;i++){
+        sum+=arr[i];
+        prod*=arr[i];
+        sum2+=1./arr[i];
+    }
+    float avg=sum/5.;
+    sum=0;
+    for(int i=0;i<5;i++){
+        sum+=pow(arr[i]-avg, 2);
 
-int main()
-{
-    cout << "Hello World!" << endl;
+    }
+
+    cout<<"Suma = "<<sum<<endl;
+    cout<<"Srednia arytmetyczna = "<<setprecision(10)<<avg<<endl;
+    cout<<"Srednia geometryczna = "<<pow(prod,1./5)<<endl;
+    cout<<"Srednia harmoniczna  = "<<5./sum2<<endl;
+    cout<<"Odchylenie standardowe  = "<<sqrt(sum/4.)<<endl;
     return 0;
 }
--- lab_04/zad_05.cpp
+++ lab_04/zad_05.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const int n=5;
+    const int m=4;
+    int arr1[n]={1,2,3,4,5};
+    int arr2[m]={6,7,8,9};
+    int arr3[n+m];
+    for(int i=0; i<n; i++){
+        arr3[i]=arr1[i];
+    }
+    for(int i=0; i<m; i++){
+        arr3[i+n]=arr2[i];
+    }
+    for(int i=0; i<n+m; i++){
+        cout<<arr3[i]<<endl;
+    }
     return 0;
 }
