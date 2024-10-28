fcccb9aa7e5eaa5fbe3a6557a5aa65d47bf9b648
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-28 16:25:49 +0100

    ZAD 15

--- lab_04/zad_15.cpp
+++ lab_04/zad_15.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
+#include <cstring>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const int size=20;
+    char napis[size];
+    int x=0;
+    cin.get(napis, size);
+    //cout<<strlen(napis);
+    for(; napis[x]!='\0'; x++){
+    }
+    cout<<x;
     return 0;
 }
