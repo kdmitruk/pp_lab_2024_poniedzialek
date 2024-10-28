b4117808cbe24c0df7b7c3f0db3f87d4f9b7128b
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-28 14:29:33 +0100

    zad 3

--- lab_04/zad_03.cpp
+++ lab_04/zad_03.cpp
@@ -4,6 +4,24 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    const int size=5;
+    int arr1[size]={1,2,3,4,5};
+    /*int arr2[size];
+    for(int i=0; i<size; i++){
+        arr2[size-1-i]=arr1[i];
+    }
+    for(int j=0; j<size; j++){
+        arr1[j]=arr2[j];
+        cout<<arr1[j]<<endl;
+    }*/
+    int t;
+    for(int i=0; i<size/2; i++){
+        t=arr1[i];
+        arr1[i]=arr1[size-1-i];
+        arr1[size-1-i]=t;
+    }
+    for(int i=0; i<size; i++){
+        cout<<arr1[i]<<endl;
+    }
     return 0;
 }
