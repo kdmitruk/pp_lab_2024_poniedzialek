4c9943658a47b58b7db7a10ff23bbf456495b1ad
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-28 15:08:38 +0100

    zadanie 9

--- lab_04/zad_09.cpp
+++ lab_04/zad_09.cpp
@@ -4,6 +4,30 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+    const int n=20;
+    int arr[n];
+    srand(time(0));
+    for(int i=0;i<n;i++)
+        arr[i]=rand()%100+1;
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<' ';
+    cout<<endl;
+
+    int temp;
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < n - i - 1; j++){
+//            if(arr[j] > arr[j+1]){
+            if(arr[j] % 2 != 0){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+                //swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++)
+        cout<<arr[i]<<' ';
+    cout<<endl;
 }
