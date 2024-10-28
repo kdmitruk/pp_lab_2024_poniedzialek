35127a357800e74d33711c0136d6d0673ff37f58
Krzysztof Dmitruk <kdmitruk@gmail.com>
2024-10-28 15:51:11 +0100

    zad. 10

--- lab_04/zad_10.cpp
+++ lab_04/zad_10.cpp
@@ -1,9 +1,30 @@
+//Napisz program, który dla tablicy liczb całkowitych wypisze z niej wartość najbliższą
+//średniej arytmetycznej wszystkich wartości z tej tablicy.
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    float avg = 0;
+    const int size=5;
+    int arr[]={6,8,3,4,5};
+    for(int i=0;i<size;i++){
+        avg += arr[i];
+    }
+    avg/=size;
+    float diff;
+    float min_diff = MAXFLOAT;
+    int index;
+    for(int i=0;i<size;i++){
+        diff = fabs(arr[i] - avg);
+        if(min_diff > diff){
+            min_diff = diff;
+            index = i;
+        }
+    }
+    cout << avg << " " << index << " " << arr[index] << endl;
+
     return 0;
 }
